2f435aba0c85a4a2a3d6f01a6e38b2663256e00e
Nathan <nthnlroman@gmail.com>
2025-11-29 16:06:01 -0600

    Fix routing for admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 // LOGIN ADMIN
 import AdminLogin from "./admin/AdminLogin";
 
-// LAYOUT ADMIN
+// LAYOUT
 import AdminLayout from "./admin/AdminLayout";
 
-// PÁGINAS DEL ADMIN
+// PÁGINAS
 import AdminDashboard from "./admin/pages/AdminDashboard";
 import HomeEditor from "./admin/pages/HomeEditor";
 import AboutEditor from "./admin/pages/AboutEditor";
@@ -14,8 +14,6 @@ import PortfolioEditor from "./admin/pages/PortfolioEditor";
 import PortfolioPhotos from "./admin/pages/PortfolioPhotos";
 import ContactEditor from "./admin/pages/ContactEditor";
 import GlobalEditor from "./admin/pages/GlobalEditor";
-
-
 
 export default function App() {
   return (
@@ -25,17 +23,21 @@ export default function App() {
         {/* LOGIN */}
         <Route path="/admin/login" element={<AdminLogin />} />
 
+        {/* REDIRECCIÓN INICIAL */}
+        <Route path="/" element={<Navigate to="/admin" replace />} />
+
         {/* ADMIN */}
         <Route path="/admin" element={<AdminLayout />}>
+
           <Route index element={<AdminDashboard />} />
           <Route path="home" element={<HomeEditor />} />
           <Route path="about" element={<AboutEditor />} />
           <Route path="portfolio" element={<PortfolioEditor />} />
           <Route path="contact" element={<ContactEditor />} />
           <Route path="global" element={<GlobalEditor />} />
-          <Route path="/admin/portfolio/:albumId/photos" element={<PortfolioPhotos />} />
-          <Route path="/admin/global" element={<GlobalEditor />} />
-          
+
+          {/* RUTAS ANIDADAS CORRECTAS */}
+          <Route path="portfolio/:albumId/photos" element={<PortfolioPhotos />} />
 
         </Route>
 
